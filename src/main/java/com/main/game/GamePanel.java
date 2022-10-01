f6d6766c665aa127ac13cedaf66e7cc4bd56fcf6
Hikanon <roma.makhanov@mail.ru>
2022-10-02 01:16:14 +0300

    Base logic

--- src/main/java/com/main/game/GamePanel.java
+++ src/main/java/com/main/game/GamePanel.java
@@ -30,22 +30,33 @@ public class GamePanel extends JPanel implements Runnable {
         gameThread = new Thread(this);
         gameThread.start();
     }
-
+//TODO Сделать нормальное ограничение фпс или одтелить привязку обновления от фпс
     @Override
     public void run() {
+
         double drawInterval = 1000000000.0/FPS;
         double delta = 0;
         long lastTime = System.nanoTime();
         long currentTime;
+        long timer = 0;
+        int drawCount = 0;
 
         while (gameThread != null){
             currentTime = System.nanoTime();
             delta += (currentTime - lastTime)/drawInterval;
+            timer += currentTime - lastTime;
             lastTime = currentTime;
             if(delta >= 1){
                 update();
                 repaint();
                 delta--;
+                drawCount++;
+            }
+
+            if(timer >= 1000000000){
+                System.out.println("FPS " + drawCount);
+                drawCount = 0;
+                timer = 0;
             }
         }
     }
